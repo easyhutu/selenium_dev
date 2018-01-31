78b520c2abfb138e79ee9174b0b351e279607df4
meng <1711621009@qq.com>
2018-01-31 20:05:13 +0800

    增加ie， firefox 驱动文件

--- config/settings.py
+++ config/settings.py
@@ -2,3 +2,25 @@
 CREAT: 2018/1/30
 AUTHOR:　HEHAHUTU
 """
+
+import os
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+ROOT_PATH = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir))
+
+BROWSER_DRIVER = {
+    'CHROME': {
+        'LINUX': os.path.abspath(os.path.join(ROOT_PATH, 'tools/driver/chrome/linux/chromedriver')),
+        'WINDOWS': os.path.abspath(os.path.join(ROOT_PATH, 'tools/driver/chrome/win32/chromedriver.exe'))
+
+    },
+    'FIREFOX': {
+        'LINUX': os.path.abspath(os.path.join(ROOT_PATH, 'tools/driver/firefox/linux/geckodriver')),
+        'WINDOWS': os.path.abspath(os.path.join(ROOT_PATH, 'tools/driver/firefox/win32/geckodriver.exe'))
+
+    },
+    'IE': {
+        'WINDOWS': os.path.abspath(os.path.join(ROOT_PATH, 'tools/driver/ie/win32/IEDriverServer.exe'))
+
+    }
+}
